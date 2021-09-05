106714629a5ff8aa7b2dea06a5e08e58d46b3786
Daniil Chernyshev <des1nteresado@outlook.com>
2021-09-05 12:32:12 +0300

    Added ValidationPipe; Updated DTOs.

--- src/breed/dto/createBreed.dto.ts
+++ src/breed/dto/createBreed.dto.ts
@@ -1,13 +1,16 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsNumber, IsString } from 'class-validator';
 
 @InputType()
 export class CreateBreedDto {
+  @IsString()
   @Field()
   name: string;
 
   @Field({ nullable: true })
   country?: string;
 
+  @IsNumber()
   @Field(() => Int)
   lifespan: number;
 }
--- src/pet/dto/createPet.dto.ts
+++ src/pet/dto/createPet.dto.ts
@@ -1,10 +1,13 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsNumberString, IsString } from 'class-validator';
 
 @InputType()
 export class CreatePetDto {
+  @IsString()
   @Field()
   name: string;
 
+  @IsNumberString()
   @Field({ nullable: true })
   breed?: string;
 }
